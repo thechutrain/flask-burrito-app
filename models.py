d676b270b054f2f0a3b5bfa818e3f089ba0c8de6
Alan Chu <chu.chuyalan@gmail.com>
2016-08-01 15:35:38 -0400

    first commit, with protect password in config.json file

---
+++ models.py
@@ -0,0 +1,35 @@
+from flask_bcrypt import generate_password_hash
+from peewee import *
+
+DATABASE = SqliteDatabase(':memory:')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = DATABASE
+
+
+class User(BaseModel):
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+
+    @classmethod
+    def create_user(cls, email, password):
+        try:
+            cls.create(
+            email=email,
+            password=generate_password_hash(password)
+            )
+        except:
+            pass
+
+
+class Taco(BaseModel):
+    protein = CharField() # chicken, beef
+    shell = CharField() # hard or soft
+    chesse = BooleanField(default=False)
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User, Taco], safe=True)
+    DATABASE.close()
---
+++ tacocat.py
@@ -0,0 +1,25 @@
+from flask import (Flask, render_template)
+import json
+
+import forms
+import models
+import passwords
+
+DEBUG = True
+PORT = 8000
+HOST = '0.0.0.0'
+
+app = Flask(__name__)
+
+# Get passwords from config in .gitignore repo
+with open('config.json', 'r') as f:
+    config = json.load(f)
+app.secret_key = config['flask_app_key']
+
+@app.route("/")
+def home_page():
+    return render_template("layout.html")
+
+if __name__ == '__main__':
+    models.initialize()
+    app.run(debug=DEBUG, host=HOST, port=PORT)
