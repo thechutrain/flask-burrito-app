5c2d3018d6a81a5fe078f631373eacfdb15efdc3
Alan Chu <chu.chuyalan@gmail.com>
2016-08-01 20:05:25 -0400

    working view for register.html, form submits with valid data

---
+++ forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import (DataRequired, Email, Length, EqualTo)
+
+
+class SignUp(Form):
+    email = StringField(
+        'Email', validators=[
+        DataRequired(),
+        Email()
+        ])
+
+    password = PasswordField(
+    'Password', validators=[
+    DataRequired(),
+    EqualTo('password2', message="Passwords much match."),
+    Length(min=5)
+    ])
+
+    password2 = PasswordField('Password2', validators=[])
--- tacocat.py
+++ tacocat.py
@@ -1,4 +1,4 @@
-from flask import (Flask, render_template)
+from flask import (Flask, render_template, redirect, url_for, flash)
 import json
 
 import forms
@@ -16,8 +16,22 @@ with open('config.json', 'r') as f:
 app.secret_key = config['flask_app_key']
 
 @app.route("/")
-def home_page():
-    return render_template("layout.html")
+def index():
+    return "Index page"
+    # return render_template("index.html", tacos=taco)
+
+@app.route("/register", methods=("POST", "GET"))
+def register():
+    form = forms.SignUp()
+    if form.validate_on_submit():
+        flash("You're just registered your account", "success")
+        models.User.create_user(
+            email=form.email.data,
+            password=form.password.data
+        )
+        return redirect(url_for("index"))
+    return render_template("register.html", form=form)
+
 
 if __name__ == '__main__':
     models.initialize()
